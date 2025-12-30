1abfd0c685ca6434a0c4cd85bc84f670457ad491
Adilson Sandoval <adilsonsdev@gmail.com>
2025-12-30 02:23:57 +0100

    add initial server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
